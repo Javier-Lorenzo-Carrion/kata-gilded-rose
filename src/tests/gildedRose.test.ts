209e2fff18904fd82d86ba2e4749beaa04e3fef6
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-06-17 14:02:35 +0100

    initial commit

--- src/tests/gildedRose.test.ts
+++ src/tests/gildedRose.test.ts
@@ -3,9 +3,7 @@ import { Item, GildedRose } from '../core/gildedRose';
 describe('The Gilded Rose', () => {
 	it('updates quality for a new item', () => {
 		const gildedRose = new GildedRose([new Item('new item', 0, 0)]);
-
 		const items = gildedRose.updateQuality();
-
-		expect(items[0]).toEqual({"name": "new item", "quality": 0, "sellIn": -1});
+		expect(items[0]).toEqual({ name: 'new item', quality: 0, sellIn: -1 });
 	});
 });
